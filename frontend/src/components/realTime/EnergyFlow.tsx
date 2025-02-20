1349c1f7a10cb9ade791a5dff17b53b5138ebe82
Miya <miya.wujing@gmail.com>
2025-02-20 13:04:29 +1300

    Feat: Complete real-time monitoring page Energy Flow content part

---
+++ frontend/src/components/realTime/EnergyFlow.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const EnergyFlow: React.FC = () => {
+    return (
+        <div className='energyFlow transparent-card flex-column-left'>
+
+            <div className='text-center mt-30 font-20'>Energy Flow</div>
+
+            <div style={{height:'60%'}}>
+                {/* energy flow */}
+            </div>
+
+            {/* content */}
+            <div className='ml-50'>
+                <div className='flex-left'>
+                    <div className='font-16'>Current Power Source:</div>
+                    {/* TODO */}
+                    <div className='font-20 ml-10'>Solar</div>
+                </div>
+
+                <div className='flex-left mt-20'>
+                    <div className='font-16'>Battery Status:</div>
+                    {/* TODO */}
+                    <div className='font-20 ml-10'>Not Charging</div>
+                </div>
+                <div className='flex-left mt-20'>
+                    <div className='font-16'>Grid Energy Transfer:</div>
+                    {/* TODO */}
+                    <div className='font-20 ml-10'>On</div>
+                </div>
+                <div className='flex-left mt-20'>
+                    <div className='font-16'>Grid Energy Purchase:</div>
+                    {/* TODO */}
+                    <div className='font-20 ml-10'>Off</div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default EnergyFlow;
