0d3c4a077666f5404eda9f85fdf0c5ea2a597361
Miya <miya.wujing@gmail.com>
2025-02-20 15:55:32 +1300

    Feat: Complete content part of real-time monitoring page-battery

---
+++ frontend/src/components/realTime/BatteryOverview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ShareIcon from '../../assets/svg/share.svg';
+// 引入电池电量图，用电图
+
+const BatteryOverview: React.FC = () => (
+    <div className='batteryOverview flex-column-left prl-20'>
+
+        <div className='two-column-layout' style={{ width: '100%' }}>
+            <div className='text-center mt-30 font-20'>Battery</div>
+            <div className='centered-content circle size-40 background-color-light '>
+                <img src={ShareIcon} alt='share' className='size-20' />
+            </div>
+        </div>
+
+        {/* maximum capacity */}
+        <div className='flex-left mt-20 '>
+            <div className='font-16'>Maximum Capacity</div>
+            {/* TODO  */}
+            <div className='font-20 ml-10'>96%</div>
+        </div>
+
+        {/* 电池电量图 */}
+        {/* 用电图 */}
+
+
+    </div>
+);
+
+export default BatteryOverview;
