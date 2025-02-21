948682ecb71f5a6c338ce67e7ebc0842c378133c
Miya <miya.wujing@gmail.com>
2025-02-22 00:10:37 +1300

    Refactor: Enhance resuable Diagram component

--- frontend/src/components/diagrams/SolarCurrentPowerOutput.tsx
+++ frontend/src/components/diagrams/SolarCurrentPowerOutput.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 
 const SolarCurrentPowerOutput: React.FC = () => {
     return (
-        <div className='flex-column width-30 height-100 transparent-card'>
-            <div className=' flex-left-center mt-20 '>
-                <div className='font-24 '>Current Power Output:</div>
-                {/* TODO */}
-                <div className='font-20 ml-10'>17kWh/h</div>
-            </div>
-            <div className='flex-column width-90 height-80 mt-30 ' style={{ backgroundColor: 'red' }}>
-                SolarCurrentPowerOutput.tsx
-            </div>
+
+        <div className='flex-column width-90 height-80 mt-30 ' style={{ backgroundColor: 'red' }}>
+            SolarCurrentPowerOutput.tsx
         </div>
+
 
     );
 };
--- frontend/src/pages/Solar.tsx
+++ frontend/src/pages/Solar.tsx
@@ -30,8 +30,16 @@ const Solar: React.FC = () => {
                     {/* top row */}
                     <div className='flex width-100 height-40 ' >
 
-                        {/* current output */}
-                        <SolarCurrentPowerOutput />
+                        {/* current output REUSE */}
+                        <div className='flex-column width-30 height-100 transparent-card'>
+                            <div className=' flex-left-center mt-20 '>
+                                <div className='font-24 '>Current Power Output:</div>
+                                {/* TODO */}
+                                <div className='font-20 ml-10'>17kWh/h</div>
+                            </div>
+                            <SolarCurrentPowerOutput />
+                        </div>
+
 
                         {/* efficiency */}
                         <SolarEfficiency />
